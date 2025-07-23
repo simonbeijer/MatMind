90b6e2ccdca90dcaa3ceb95b16e6b400d20aae71
”simobeijer” <beijer.simon@gmail.com>
2025-07-23 16:28:59 +0200

    Add Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConditionalFooter from "./components/conditionalFooter";
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
           {children}
           <ConditionalFooter />
+          <Analytics />
       </body>
     </html>
   );
